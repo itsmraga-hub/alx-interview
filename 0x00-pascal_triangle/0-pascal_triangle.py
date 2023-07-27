0ab1378a378b2e6992386a627e1a634928bc9f8a
itsmraga-hub <ragawilliam570@gmail.com>
2023-07-27 19:50:30 +0300

    Fix code fail

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def pascal_triangle(n):
-    """Returns a list of lists of integers representing the
+    """
+        Returns a list of lists of integers representing the
         Pascal's triangle of n
     """
     if n <= 0:
