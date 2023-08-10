106e2d95a2ccaa89a1281cf12e88122a958d1e0d
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-10 23:44:46 +0300

    Fix pycodestyle errors

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -10,25 +10,26 @@
     If n is impossible to achieve, return 0
 """
 
+
 def minOperations(n):
     """
         minimum operations solution done returning the number of operations
     """
     if n <= 1:
         return 0
-    
+
     # Initialize factors and operations
     factors = []
     operations = 0
-    
+
     # Find factors of n
     for i in range(2, n + 1):
         while n % i == 0:
             factors.append(i)
             n //= i
-    
+
     # Calculate the sum of factors
     for factor in factors:
         operations += factor
-    
+
     return operations
